f2ebfd8711d867ce9ee176a2d876262b2fc4267f
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-10-13 16:31:32 +0100

    Migration added

---
+++ EmployeeManagementApp/Data/Migrations/20241013152926_InitialCreate.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EmployeeManagementApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentCode = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    DepartmentName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployeeName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Position = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    DepartmentCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_DepartmentCode",
+                        column: x => x.DepartmentCode,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentCode",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attendances",
+                columns: table => new
+                {
+                    AttendanceId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployeeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AttendanceDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendances", x => x.AttendanceId);
+                    table.ForeignKey(
+                        name: "FK_Attendances_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentCode", "DepartmentName" },
+                values: new object[,]
+                {
+                    { "FN", "Finance" },
+                    { "HR", "Human Resources" },
+                    { "IT", "Information Technology" },
+                    { "MK", "Marketing" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DepartmentCode", "EmployeeName", "Position" },
+                values: new object[,]
+                {
+                    { 1, "HR", "John Doe", "Manager" },
+                    { 2, "IT", "Jane Smith", "Software Developer" },
+                    { 3, "FN", "Samuel Lee", "Accountant" },
+                    { 4, "MK", "Emma Brown", "Marketing Specialist" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Attendances",
+                columns: new[] { "AttendanceId", "AttendanceDate", "EmployeeId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 2, new DateTime(2024, 10, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 3, new DateTime(2024, 10, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 4, new DateTime(2024, 10, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 5, new DateTime(2024, 10, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 3 },
+                    { 6, new DateTime(2024, 10, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 3 },
+                    { 7, new DateTime(2024, 10, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 4 },
+                    { 8, new DateTime(2024, 10, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendances_EmployeeId",
+                table: "Attendances",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentCode",
+                table: "Employees",
+                column: "DepartmentCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendances");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
